121612718529726c096b26e991d469103bd1df60
IgnacioCarnero <nachocarnero77@gmail.com>
2023-09-04 13:26:14 -0300

    Primeros cambios en archivo

--- app/src/main/java/com/example/food_app/MainActivity.java
+++ app/src/main/java/com/example/food_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.food_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-  
     }
+    public void btnSaludo(View v) {
+            Toast.makeText(this, "Holaaaa!", Toast.LENGTH_LONG).show();
+    }
+  
 }
