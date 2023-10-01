db8617ad63a94a40771da5f45a8df0d2203d416d
Florencia Morisse <florenciamorisse@gmail.com>
2023-09-30 23:52:00 -0300

    agregué imágenes y creé las 2 primeras activities

--- app/src/main/java/com/example/food_app/MainActivity.java
+++ app/src/main/java/com/example/food_app/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.food_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_welcome= findViewById(R.id.btn_welcome);
+        btn_welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent( MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
 
   
     }
