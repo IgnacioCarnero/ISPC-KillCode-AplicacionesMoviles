9ab27564d39f0c253f613018db1b9d2067bba797
Romherre <98140472+Romherre@users.noreply.github.com>
2023-10-30 00:05:31 -0300

    Se realizan modificaciones para alinearnos a lo requerido en accesibilidad y se conecta el login con el menu principal

--- app/src/main/java/com/example/food_app/NewPassword.java
+++ app/src/main/java/com/example/food_app/NewPassword.java
@@ -1,14 +1,28 @@
 package com.example.food_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class NewPassword extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_password);
+
+        // Obtener referencia al botón de restablecimiento de contraseña
+        Button resetPasswordButton = findViewById(R.id.buttonResetPassword);
+
+        // Configurar un OnClickListener para el botón
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cuando se hace clic en el botón, crear un Intent para ir a LoginActivity
+                Intent intent = new Intent(NewPassword.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
