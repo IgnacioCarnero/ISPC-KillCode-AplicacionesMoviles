43fea109fcdc404af3ce39d5006bb50b98da10e1
Florencia Morisse <florenciamorisse@gmail.com>
2024-05-23 13:33:37 -0300

    mejoras con Material design 3

--- app/src/main/java/com/example/food_app/SplashActivity.java
+++ app/src/main/java/com/example/food_app/SplashActivity.java
@@ -23,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 1000);
+        }, 2000);
 
     }
 }
