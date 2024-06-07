44f4ea9871a08edb219616eac40df226d131d5b3
Florencia Morisse <florenciamorisse@gmail.com>
2024-06-07 16:21:55 -0300

    cambios

--- app/src/main/java/com/example/food_app/menu/OrderConfirmationActivity.java
+++ app/src/main/java/com/example/food_app/menu/OrderConfirmationActivity.java
@@ -12,7 +12,7 @@ import com.example.food_app.R;
 
 public class OrderConfirmationActivity extends AppCompatActivity {
 
-    Button btn_demora;
+
     ImageButton btn_backmenuselection;
 
 
@@ -22,18 +22,11 @@ public class OrderConfirmationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_confirmation);
 
-        btn_demora= findViewById(R.id.btn_demora);
         btn_backmenuselection=findViewById(R.id.btn_backmenuselection);
 
 
 
-        btn_demora.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OrderConfirmationActivity.this, EstimatedtimeActivity.class);
-                startActivity(intent);
-            }
-        });
+
         btn_backmenuselection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
